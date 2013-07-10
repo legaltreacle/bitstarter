d8ae0b35cea7979db66c142325af48c3141967a2
legaltreacle <jules0@gmail.com>
2013-07-10 13:45:54 +0000

    Final revision

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+var htmlbuf=fs.readFileSync('index.html',0) //load up buffer of index.html
+var htmltxt=htmlbuf.toString(0,0) //convert above to str, start @ 0th char.
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(htmltxt);
 });
 
 var port = process.env.PORT || 5000;
