6991a165d2f5ac22393c20291e0f9517062ba60c
legaltreacle <jules0@gmail.com>
2013-07-24 17:46:52 +0100

    Final version of grader.js

--- grader.js
+++ grader.js
@@ -1,41 +1,126 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require ('util');
+var request = require ('request');
+var existStatus = ["SITE DOES NOT EXIST\n**OR** IT MAY BE UNREACHABLE CURRENTLY.\nPLEASE CHECK YOUR INTERNET CONNECTION AND DESIRED URL...\nAND TRY AGAIN LATER.", ": THIS FILE EXISTS HAS BEEN LOADED INTO MEMORY: "];
 
-var assertFileExists = function(infile) {
+//Check for checks.json substitute
+var checkChecks = function(infile) {
+
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
+
+     if(fs.existsSync(instr)) 
+ {console.log(existStatus[1]+infile);}
+
+		else{
+
+	request//start request
+		(instr, function // set parameters: where to look, then need to do stuff to what is returned. 
+				(error, response, body){ //will get parameters error, response, body so can use these.
+
+		if (!error && response.statusCode == 200){ //If you don't have an error && have a response.statusCode returned as 200, page exists...
+		console.log(existStatus[1]+infile);				
+		return instr;
+		}//endif
+
+
+	}//end subfunction
+  )//end request (a type of function)
+
+
+	        console.log("%s does not exist. Exiting.", instr);
+        	process.exit(1);
+	        }
+
     return instr;
+
+};//cChecks
+
+
+
+checkUrl = function(infile){
+
+   console.log("FUNCTION INITIATED:\ncheckUrl = function(infile):")
+   console.log("url to be checked is "+infile+"\n");
+	
+	//Confirm URL Existence in loop:
+
+	request//start request
+		(infile, function // set parameters: where to look, then need to do stuff to what is returned. 
+				(error, response, body){ //will get parameters error, response, body so can use these.
+
+		if (!error && response.statusCode == 200){ //If you don't have an error && have a response.statusCode returned as 200, you've got the page!
+		console.log(existStatus[1]+infile);		
+		
+		var cheeryUrl=cheerio.load(body);
+		console.log(cheeryUrl);
+		console.log("The above is load.cheerio(<body> of site you loaded ("+infile+", in case you'd forgotten!)");
+
+  //Now to do what we require:
+
+		//load in the local checksfile:
+		var checkyfile = (JSON.parse(fs.readFileSync(program.checks))).sort();
+		console.log("\nYou've got the following checks file loaded now:\n"+checkyfile);
+
+		var checks = checkyfile.sort(); 
+
+		$ = cheeryUrl;
+		var out = {};
+	        
+		for(var ii in checks) {
+		        var present = $(checks[ii]).length > 0;
+		        out[checks[ii]] = present;
+			}
+		
+
+	    var outJson = JSON.stringify(out, null, 4);
+	    console.log(outJson);
+		fs.writeFileSync("grade.txt",outJson);
+		console.log("Above written to grade.txt!")
+		
+		}//endif
+
+		else{
+		console.log(existStatus[0]+"\n"+error);
+		}//endelse
+
+	}//end subfunction
+  )//end request (a type of function)
+/*And to wrap up:*/
+}//end function
+
+
+
+// 
+var fileCheck = function(infile) {
+//
+    console.log("FUNCTION RUNNING:\nfileCheck = function(infile)")
+
+    var infile = infile.toString(); //ensure that we are dealing with a string and...
+	console.log("File being tested: "+infile);
+
+    	if (!fs.existsSync(infile))	{
+        console.log("%s does not exist. Exiting.", infile);
+        process.exit(1); /*http://nodejs.org/api/process.html#process_process_exit_code*/	}
+
+        //Assuming that we haven't exited by this point:
+
+	console.log("FILE "+infile+" exists.");
+        console.log("\nEntering MAIN LOOP\n");
+
+	    var checkJson = checkHtmlFile(infile, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	    console.log("<END>");
+
 };
 
+//
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -55,21 +140,26 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var clone = function(fn) {
+var clone = function(fn, infile) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
+//
+//
 
+
+//Program starts running here:
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(checkChecks), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(fileCheck), HTMLFILE_DEFAULT)
+	.option('-url, --url <url_ref>', 'URL of index.html', checkUrl) //url case allowed for. Set up function to check existence/start loading. 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+} //for require.main
+else 
+{
     exports.checkHtmlFile = checkHtmlFile;
 }
 
